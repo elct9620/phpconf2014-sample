748d34c9dd8f9b6509468dba132f631095a5fb31
蒼時弦也 <elct9620@frost.tw>
2014-10-10 16:44:45 +0800

    Add bootstrap.php for initialzie application

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Bootstrap
+ *
+ * Load project and initialize
+ */
+
+require 'vendor/autoload.php';
+
+# Setup Database
+// TODO: Add idiorm configure
+
+# Setup View
+// TODO: Add Twig configure
+
+# Setup Router
+// TODO: Add Pux configure
+
+# Boot Application
+// TODO: Let application work
